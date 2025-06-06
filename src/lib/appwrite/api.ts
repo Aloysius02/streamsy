d40dc8ade9d251204e632492f27d37c4a0fb58b6
Micheal-Repo <michealfullstack@gmail.com>
2025-06-06 11:25:09 +0100

    error fixing

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,21 @@
-import { ID, Query } from "appwrite";
+import {
+  ID,
+  Query
+} from "appwrite";
 
-import { appwriteConfig, account, databases, storage, avatars } from "./config";
-import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
+import {
+  appwriteConfig,
+  account,
+  databases,
+  storage,
+  avatars
+} from "./config";
+import {
+  IUpdatePost,
+  INewPost,
+  INewUser,
+  IUpdateUser
+} from "@/types";
 
 // ============================================================
 // AUTH
@@ -10,6 +24,7 @@ import { IUpdatePost, INewPost, INewUser, IUpdateUser } from "@/types";
 // ============================== SIGN UP
 export async function createUserAccount(user: INewUser) {
   try {
+    console.log("creating user")
     const newAccount = await account.create(
       ID.unique(),
       user.email,
@@ -17,11 +32,15 @@ export async function createUserAccount(user: INewUser) {
       user.name
     );
 
-    if (!newAccount) throw Error;
+    if (!newAccount) throw Error("unable to create user");
 
+    console.log("user created")
+
+    console.log("getting user avatarUrl")
     const avatarUrl = avatars.getInitials(user.name);
 
-    const newUser = await saveUserToDB({
+    console.log("saving user")
+    const newUser = await saveUserToDB( {
       accountId: newAccount.$id,
       name: newAccount.name,
       email: newAccount.email,
@@ -29,8 +48,10 @@ export async function createUserAccount(user: INewUser) {
       imageUrl: avatarUrl,
     });
 
+    console.log("saved user")
     return newUser;
   } catch (error) {
+    console.log("error in createUserAccount");
     console.log(error);
     return error;
   }
@@ -54,12 +75,15 @@ export async function saveUserToDB(user: {
 
     return newUser;
   } catch (error) {
+    console.log("error in saveUserToDB")
     console.log(error);
   }
 }
 
 // ============================== SIGN IN
-export async function signInAccount(user: { email: string; password: string }) {
+export async function signInAccount(user: {
+  email: string; password: string
+}) {
   try {
     const session = await account.createEmailSession(user.email, user.password);
 
@@ -201,7 +225,9 @@ export async function deleteFile(fileId: string) {
   try {
     await storage.deleteFile(appwriteConfig.storageId, fileId);
 
-    return { status: "ok" };
+    return {
+      status: "ok"
+    };
   } catch (error) {
     console.log(error);
   }
@@ -224,8 +250,13 @@ export async function searchPosts(searchTerm: string) {
   }
 }
 
-export async function getInfinitePosts({ pageParam }: { pageParam: number }) {
-  const queries: any[] = [Query.orderDesc("$updatedAt"), Query.limit(9)];
+export async function getInfinitePosts({
+  pageParam
+}: {
+  pageParam: number
+}) {
+  const queries: any[] = [Query.orderDesc("$updatedAt"),
+    Query.limit(9)];
 
   if (pageParam) {
     queries.push(Query.cursorAfter(pageParam.toString()));
@@ -287,7 +318,11 @@ export async function updatePost(post: IUpdatePost) {
         throw Error;
       }
 
-      image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id };
+      image = {
+        ...image,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id
+      };
     }
 
     // Convert tags into array
@@ -344,7 +379,9 @@ export async function deletePost(postId?: string, imageId?: string) {
 
     await deleteFile(imageId);
 
-    return { status: "Ok" };
+    return {
+      status: "Ok"
+    };
   } catch (error) {
     console.log(error);
   }
@@ -401,7 +438,9 @@ export async function deleteSavedPost(savedRecordId: string) {
 
     if (!statusCode) throw Error;
 
-    return { status: "Ok" };
+    return {
+      status: "Ok"
+    };
   } catch (error) {
     console.log(error);
   }
@@ -508,7 +547,11 @@ export async function updateUser(user: IUpdateUser) {
         throw Error;
       }
 
-      image = { ...image, imageUrl: fileUrl, imageId: uploadedFile.$id };
+      image = {
+        ...image,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id
+      };
     }
 
     //  Update user
